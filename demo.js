30cffdbde168b16bae774774c1f66c82722a7886
Jason Chan <jason@Jasons-Air.home>
2023-05-30 16:53:07 +0100

    this commit will complete finding, creating and deleting a database

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require("mongodb");
 
+const data = require("../backend-portfolio/data/personal-information.json");
+
 async function main() {
   const uri =
     "mongodb+srv://jasonchan1201:jYXCCIQOIDL95fXl@jasons-backend-portfoli.n3cirox.mongodb.net/";
@@ -8,11 +10,45 @@ async function main() {
 
   try {
     await client.connect();
+    await deleteDatabase(client);
+    await createDatabase(client);
+    await findDatabase(client);
   } catch (e) {
     console.log(e);
   } finally {
     await client.close();
   }
+}
+
+async function createDatabase(client) {
+  const result = await client
+    .db("portfolio")
+    .collection("personal-information")
+    .insertOne(data);
+
+  console.log("New data added with the following id ", result.insertedId);
+}
+
+async function findDatabase(client) {
+  const result = await client
+    .db("portfolio")
+    .collection("personal-information")
+    .find();
+
+  if (result) {
+    return result.forEach((answer) => console.log(answer));
+  } else {
+    console.log("Didn't find result");
+  }
+}
+
+async function deleteDatabase(client) {
+  const result = await client
+    .db("portfolio")
+    .collection("personal-information")
+    .deleteOne();
+
+  console.log(`${result.deletedCount} document(s) was deleted`);
 }
 
 main().catch(console.error);
